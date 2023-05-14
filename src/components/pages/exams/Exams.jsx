04efe364df7c109c5d42a08d9bef298b98fe2ccd
temirlan <tb@plazz.ag>
2023-05-14 18:34:17 +0200

    some changes

--- src/components/pages/exams/Exams.jsx
+++ src/components/pages/exams/Exams.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Services_data} from "./data.js"
+import {Exam_data} from "../../../data/data.js"
 import classes from "./Exams.module.css"
 
 const Exams = () => {
@@ -10,31 +10,30 @@ const Exams = () => {
               
          <div className={classes.serviceDetail_container}>
            <div className={classes.serviceDetail_main__info}> 
-             <h1></h1>
-             <div className={classes.serviceDetail_photo}><img src={Services_data.backImg} alt="nature"  />
+             <div className={classes.serviceDetail_photo}><img src={Exam_data.backImg} alt="nature"  />
              </div>
    
-             <h3 className={classes.serviceDetail_subtitle}>{Services_data.title}</h3>
-             <p className={classes.serviceDetail_text}> {Services_data.text}</p> 
-             <p className={classes.serviceDetail_text} >{Services_data.p1}</p>
-             <p className={classes.serviceDetail_text} >{Services_data.p2}</p>
-             <p className={classes.serviceDetail_text} >{Services_data.p3}</p>
+             <h3 className={classes.serviceDetail_subtitle}>{Exam_data.title}</h3>
+             <p className={classes.serviceDetail_text}> {Exam_data.text}</p> 
+             <p className={classes.serviceDetail_text} >{Exam_data.p1}</p>
+             <p className={classes.serviceDetail_text} >{Exam_data.p2}</p>
+             <p className={classes.serviceDetail_text} >{Exam_data.p3}</p>
    
-             <h3 className={classes.serviceDetail_subtitle}>{Services_data.subtitle}</h3>
-             <p className={classes.serviceDetail_text2}>{Services_data.task1}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task2}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task3}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task4}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task5}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task6}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task7}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task8}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task9}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task10}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task11}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task12}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task13}</p>
-             <p className={classes.serviceDetail_text2} >{Services_data.task14}</p>
+             <h3 className={classes.serviceDetail_subtitle}>{Exam_data.subtitle}</h3>
+             <p className={classes.serviceDetail_text2}>{Exam_data.task1}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task2}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task3}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task4}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task5}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task6}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task7}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task8}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task9}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task10}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task11}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task12}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task13}</p>
+             <p className={classes.serviceDetail_text2} >{Exam_data.task14}</p>
           </div>  
          </div> 
        
--- src/components/pages/schedule/Schedule.jsx
+++ src/components/pages/schedule/Schedule.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./Schedule.module.css"
-import {Services_data} from '../../../data/data.js';
+import {Schedule_data} from '../../../data/data.js';
  
 const Schedule = () => {
     return(   
@@ -12,32 +12,32 @@ const Schedule = () => {
           <div className={classes.serviceDetail_main__info}> 
             <h1></h1>
             <div>
-              <img className={classes.serviceDetail_photo} src={Services_data.backImg} alt="nature"/>
+              <img className={classes.serviceDetail_photo} src={Schedule_data.backImg} alt="nature"/>
             </div>
   
 
         
-            <h3 className={classes.serviceDetail_subtitle}>{Services_data.title}</h3>
-            <p className={classes.serviceDetail_text}> {Services_data.text}</p> 
-            <p className={classes.serviceDetail_text} >{Services_data.p1}</p>
-            <p className={classes.serviceDetail_text} >{Services_data.p2}</p>
-            <p className={classes.serviceDetail_text} >{Services_data.p3}</p>
+            <h3 className={classes.serviceDetail_subtitle}>{Schedule_data.title}</h3>
+            <p className={classes.serviceDetail_text}> {Schedule_data.text}</p> 
+            <p className={classes.serviceDetail_text} >{Schedule_data.p1}</p>
+            <p className={classes.serviceDetail_text} >{Schedule_data.p2}</p>
+            <p className={classes.serviceDetail_text} >{Schedule_data.p3}</p>
   
-            <h3 className={classes.serviceDetail_subtitle}>{Services_data.subtitle}</h3>
-            <p className={classes.serviceDetail_text2}>{Services_data.task1}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task2}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task3}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task4}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task5}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task6}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task7}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task8}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task9}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task10}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task11}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task12}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task13}</p>
-            <p className={classes.serviceDetail_text2} >{Services_data.task14}</p>
+            <h3 className={classes.serviceDetail_subtitle}>{Schedule_data.subtitle}</h3>
+            <p className={classes.serviceDetail_text2}>{Schedule_data.task1}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task2}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task3}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task4}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task5}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task6}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task7}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task8}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task9}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task10}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task11}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task12}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task13}</p>
+            <p className={classes.serviceDetail_text2} >{Schedule_data.task14}</p>
 
            
          </div>  
