4b7e28c93618e883f63a7b1680136a8770e7a4a6
temirlan <tb@plazz.ag>
2023-05-29 01:46:00 +0200

    registration scss added

--- src/components/pages/rigistration/index.jsx
+++ src/components/pages/rigistration/index.jsx
@@ -5,20 +5,18 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
 
-import styles from "./Registration.module.css";
-
 const Registration = () => {
   return (
-    <Paper classes={{ root: styles.root }}>
-      <Typography classes={{ root: styles.title }} variant="h5">
+    <Paper className="registration_root">
+      <Typography className="registration_title" variant="h5">
         Создание аккаунта
       </Typography>
-      <div className={styles.avatar}>
+      <div className="registration_avatar">
         <Avatar sx={{ width: 100, height: 100 }} />
       </div>
-      <TextField className={styles.field} label="Полное имя" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Пароль" fullWidth />
+      <TextField className="registration_field" label="Полное имя" fullWidth />
+      <TextField className="registration_field" label="E-Mail" fullWidth />
+      <TextField className="registration_field" label="Пароль" fullWidth />
       <Button size="large" variant="contained" fullWidth>
         Зарегистрироваться
       </Button>
