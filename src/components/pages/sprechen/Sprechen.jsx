f633cfa0b4c2e6e0e9ed0a4ebd3d2b9bce149526
temirlan <tb@plazz.ag>
2023-05-29 01:59:04 +0200

    sprechen scss added

--- src/components/pages/sprechen/Sprechen.jsx
+++ src/components/pages/sprechen/Sprechen.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import classes from "./Sprechen.module.css";
 import { Sprechen_data } from "../../../data/data.js";
 import OneSprechen from "./OneSprechen";
 
 const Sprechen = () => {
   return (
     <div className="bigWrapper">
-      <section className={classes.services}>
-        <div className={classes.wrapper}>
-          <h2 className={classes.services_title}>Sprechen</h2>
+      <section className="services">
+        <div className="wrapper">
+          <h2 className="services_title">Sprechen</h2>
 
-          <div className={classes.service__cards}>
+          <div className="service__cards">
             {Sprechen_data.map((services) => (
               <OneSprechen services={services} />
             ))}
