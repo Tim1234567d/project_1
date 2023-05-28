6a5a989b6937319e5cfec7957eb40de65f32c07b
temirlan <tb@plazz.ag>
2023-05-29 01:53:17 +0200

    schreiben scss added

--- src/components/pages/schreiben/Schreiben.jsx
+++ src/components/pages/schreiben/Schreiben.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import classes from "./Schreiben.module.css";
 import { Schreiben_data } from "../../../data/data.js";
 import OneSchreiben from "./OneSchreiben";
 
 const Schreiben = () => {
   return (
     <div className="bigWrapper">
-      <section className={classes.services}>
-        <div className={classes.wrapper}>
-          <h2 className={classes.services_title}>Schreiben</h2>
+      <section className="services">
+        <div className="wrapper">
+          <h2 className="services_title">Schreiben</h2>
 
-          <div className={classes.service__cards}>
+          <div className="service__cards">
             {Schreiben_data.map((services) => (
               <OneSchreiben services={services} />
             ))}
