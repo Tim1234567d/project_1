bf9fe0ecfd981c5987fd6a1d62aa9e394ccdcc30
temirlan <tb@plazz.ag>
2023-05-29 01:18:25 +0200

    horen scss added

--- src/components/pages/horen/Horen.jsx
+++ src/components/pages/horen/Horen.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import classes from "./Horen.module.css";
 import { Horen_data } from "../../../data/data.js";
 import OneHoren from "./OneHoren";
 
 const Horen = () => {
   return (
     <div className="bigWrapper">
-      <section className={classes.services}>
-        <div className={classes.wrapper}>
-          <h2 className={classes.services_title}>Horen</h2>
+      <section className="services">
+        <div className="wrapper">
+          <h2 className="services_title">Horen</h2>
 
-          <div className={classes.service__cards}>
+          <div className="service__cards">
             {Horen_data.map((services) => (
               <OneHoren services={services} />
             ))}
--- src/components/pages/horen/OneHoren.jsx
+++ src/components/pages/horen/OneHoren.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import classes from "./Horen.module.css";
 
 const OneHoren = ({ services }) => {
   return (
     <div key={services.id}>
       <Link to={`/module1/${services.id}`}>
-        <div className={classes.service__card}>
+        <div className="service__card">
           <img
-            className={classes.service_backImg}
+            className="service_backImg"
             src={services.backImg}
             alt="service img"
           />
-          <div className={classes.service_title_wrapper}>
-            <h3 className={classes.service_title}>{services.title}</h3>
+          <div className="service_title_wrapper">
+            <h3 className="service_title">{services.title}</h3>
           </div>
         </div>
       </Link>
