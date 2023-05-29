d336b5f977e4db833b671d704443b551ea8c17bd
temirlan <tb@plazz.ag>
2023-05-29 02:38:41 +0200

    schreiben part fixed

--- src/App.js
+++ src/App.js
@@ -16,16 +16,19 @@ import Schreiben from "./components/pages/schreiben/Schreiben";
 import Sprechen from "./components/pages/sprechen/Sprechen";
 
 // ** module
-import ModuleSchreiben from "./components/pages/schreiben/Module_schreiben";
+// import ModuleSchreiben from "./components/pages/schreiben/Module_schreiben";
 import ModuleSprechen from "./components/pages/sprechen/Module_sprechen";
 // import ModuleHoren from "./components/pages/horen/Module_horen";
 
 import HorenModule1 from "./components/pages/horen/horenModule/HorenModule1";
 import LesenModule1 from "./components/pages/lesen/lesenModule/SubLesenModule1";
+import SchreibenModule1 from "./components/pages/schreiben/schreibenModule/SchreibenModule1";
+
 
 //  ** module part
 import LesenTeilen from "./components/pages/lesen/lesenModule/LesenTeilen";
 import HorenPart from "./components/pages/horen/horenModule/HorenPart";
+import SchreibenPart from "./components/pages/schreiben/schreibenModule/SchreibenPart";
 
 function App() {
   return (
@@ -49,13 +52,15 @@ function App() {
         {/* <Route path="/lesenteilen/:id/:id" element = {<Module/> } /> */}
         <Route path="/lesenModule1" element={<LesenModule1 />} />
         <Route path="horenModule1" element={<HorenModule1 />} />
+        <Route path="schreibenModule1" element={<SchreibenModule1 />} />
 
-        <Route path="/module2/:id" element={<ModuleSchreiben />} />
+        {/* <Route path="/module2/:id" element={<ModuleSchreiben />} /> */}
         <Route path="/module3/:id" element={<ModuleSprechen />} />
 
         {/* ** Module  part*/}
         <Route path="/lesenModule1/:id" element={<LesenTeilen />} />
         <Route path="/horenModule1/:id" element={<HorenPart />} />
+        <Route path="/schreibenModule1/:id" element={<SchreibenPart />} />
       </Routes>
       <Footer />
     </div>
--- src/components/pages/schreiben/Schreiben.jsx
+++ src/components/pages/schreiben/Schreiben.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Schreiben_data } from "../../../data/data.js";
-import OneSchreiben from "./OneSchreiben";
+import { Link } from "react-router-dom";
 
 const Schreiben = () => {
   return (
@@ -10,9 +9,18 @@ const Schreiben = () => {
           <h2 className="services_title">Schreiben</h2>
 
           <div className="service__cards">
-            {Schreiben_data.map((services) => (
-              <OneSchreiben services={services} />
-            ))}
+            <Link to="/schreibenModule1">
+              <div className="service__card">
+                <img
+                  className="service_backImg"
+                  src="https://media.istockphoto.com/id/1284691550/de/vektor/blaue-abstrakte-geometrische-dynamische-form-papier-schichten-subtilen-hintergrundvektor.jpg?s=612x612&w=0&k=20&c=u3jENSNjf1hW7G4Q0Uxz05qCkul-IvVu63J9oVBk16A="
+                  alt="service img"
+                />
+                <div className="service_title_wrapper">
+                  <h3 className="service_title">Schreiben module 1</h3>
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
       </section>
