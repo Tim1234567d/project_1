ae307c3c4cc55df337e18aea2da4bf37142a4076
temirlan <tb@plazz.ag>
2023-05-30 10:36:55 +0200

    exams style fixed

--- src/components/pages/exams/Exams.jsx
+++ src/components/pages/exams/Exams.jsx
@@ -1,45 +1,36 @@
 import React from "react";
 import { Exam_data } from "../../../data/data.js";
 
-
 const Exams = () => {
   return (
-    <div className="bigWrapper">
-      <div className="serviceDetail_wrapper">
-        <div className="serviceDetail_container">
-          <div className="serviceDetail_main__info">
-            <div className="serviceDetail_photo">
-              <img src={Exam_data.backImg} alt="nature" />
-            </div>
+    <div className="wrapper">
+      <section className="exams_contioner">
+        <img className="exams_photo" src={Exam_data.backImg} alt="nature" />
 
-            <h3 className="serviceDetail_subtitle">
-              {Exam_data.title}
-            </h3>
-            <p className="serviceDetail_text"> {Exam_data.text}</p>
-            <p className="serviceDetail_text">{Exam_data.p1}</p>
-            <p className="serviceDetail_text">{Exam_data.p2}</p>
-            <p className="serviceDetail_text">{Exam_data.p3}</p>
+        <h3 className="exams_subtitle">{Exam_data.title}</h3>
 
-            <h3 className="serviceDetail_subtitle">
-              {Exam_data.subtitle}
-            </h3>
-            <p className="serviceDetail_text2">{Exam_data.task1}</p>
-            <p className="serviceDetail_text2">{Exam_data.task2}</p>
-            <p className="serviceDetail_text2">{Exam_data.task3}</p>
-            <p className="serviceDetail_text2">{Exam_data.task4}</p>
-            <p className="serviceDetail_text2">{Exam_data.task5}</p>
-            <p className="serviceDetail_text2">{Exam_data.task6}</p>
-            <p className="serviceDetail_text2">{Exam_data.task7}</p>
-            <p className="serviceDetail_text2">{Exam_data.task8}</p>
-            <p className="serviceDetail_text2">{Exam_data.task9}</p>
-            <p className="serviceDetail_text2">{Exam_data.task10}</p>
-            <p className="serviceDetail_text2">{Exam_data.task11}</p>
-            <p className="serviceDetail_text2">{Exam_data.task12}</p>
-            <p className="serviceDetail_text2">{Exam_data.task13}</p>
-            <p className="serviceDetail_text2">{Exam_data.task14}</p>
-          </div>
-        </div>
-      </div>
+        <p className="exams_text"> {Exam_data.text}</p>
+        <p className="exams_text">{Exam_data.p1}</p>
+        <p className="exams_text">{Exam_data.p2}</p>
+        <p className="exams_text">{Exam_data.p3}</p>
+
+        <h3 className="exams_subtitle">{Exam_data.subtitle}</h3>
+
+        <p className="exams_text2">{Exam_data.task1}</p>
+        <p className="exams_text2">{Exam_data.task2}</p>
+        <p className="exams_text2">{Exam_data.task3}</p>
+        <p className="exams_text2">{Exam_data.task4}</p>
+        <p className="exams_text2">{Exam_data.task5}</p>
+        <p className="exams_text2">{Exam_data.task6}</p>
+        <p className="exams_text2">{Exam_data.task7}</p>
+        <p className="exams_text2">{Exam_data.task8}</p>
+        <p className="exams_text2">{Exam_data.task9}</p>
+        <p className="exams_text2">{Exam_data.task10}</p>
+        <p className="exams_text2">{Exam_data.task11}</p>
+        <p className="exams_text2">{Exam_data.task12}</p>
+        <p className="exams_text2">{Exam_data.task13}</p>
+        <p className="exams_text2">{Exam_data.task14}</p>
+      </section>
     </div>
   );
 };
